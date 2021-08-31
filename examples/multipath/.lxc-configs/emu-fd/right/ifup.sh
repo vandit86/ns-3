3522e2c98dd1711fb27edef0fdc186d35d4fd0de
vandit86 <vandit86@mail.ru>
2021-08-31 15:41:41 +0100

    changes on LXC config files, start working with SUMO

--- examples/multipath/.lxc-configs/emu-fd/right/ifup.sh
+++ examples/multipath/.lxc-configs/emu-fd/right/ifup.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
-#echo "ip route add 15.0.0.0/8 via 14.0.0.1 dev eth1"
-ip route add 11.0.0.3/32 via 13.0.0.1 dev eth1
-ip route add 11.0.0.0/8 via 13.0.0.1 dev eth0
-ip route add default via 13.0.0.1
+ip route add 15.0.0.0/8 via 14.0.0.1 dev eth1
+ip route add 11.0.0.0/8 via 13.0.0.1 dev eth0 
+
+# disable checksum 
+ethtool --offload  eth0  rx off  tx off
+ethtool --offload  eth1  rx off  tx off
+
+# do not need line below , just to ping test
+ip route add 16.0.0.0/8 via 14.0.0.1 dev eth1
+
+# tc add delay to interface
+tc qdisc add dev eth0 root netem delay 55ms
+tc qdisc add dev eth1 root netem delay 20ms
+
